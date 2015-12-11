5d41b17e28c8a9babb22e27b111b0a846e8f76f5
Chowdhury Yeameen <yeameen@qualtrics.com>
2015-12-11 11:57:57 -0800

    Moving security configuration to Java according to comment:
    http://jhasaket.blogspot.com/2014/09/securing-spring-mvc-application-using.html?showComment=1424901617140#c5444745786600701461

--- src/main/java/com/rst/oauth2/google/config/OAuth2SecurityConfiguration.java
+++ src/main/java/com/rst/oauth2/google/config/OAuth2SecurityConfiguration.java
@@ -2,6 +2,9 @@ package com.rst.oauth2.google.config;
 
 import static com.google.common.collect.Lists.newArrayList;
 
+import com.rst.oauth2.google.security.DefaultUserAuthenticationConverter;
+import com.rst.oauth2.google.security.GoogleAccessTokenConverter;
+import com.rst.oauth2.google.security.GoogleTokenServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -9,13 +12,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.AccessTokenRequest;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.common.AuthenticationScheme;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 import javax.annotation.Resource;
 import java.util.Collections;
@@ -23,13 +33,16 @@ import java.util.List;
 
 @Configuration
 @EnableOAuth2Client
-class OAuth2SecurityConfiguration {
+class OAuth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private Environment env;
 
     @Resource
     @Qualifier("accessTokenRequest")
     private AccessTokenRequest accessTokenRequest;
+
+    @Autowired
+    OAuth2ClientContextFilter oauth2ClientContextFilter;
 
     @Bean
     @Scope("session")
@@ -50,15 +63,80 @@ class OAuth2SecurityConfiguration {
         return details;
     }
 
-    private List<String> parseScopes(String commaSeparatedScopes) {
-        List<String> scopes = newArrayList();
+    public OAuth2SecurityConfiguration() {
+        super(true);
+    }
+
+    private List parseScopes(String commaSeparatedScopes) {
+        List scopes = newArrayList();
         Collections.addAll(scopes, commaSeparatedScopes.split(","));
         return scopes;
     }
 
     @Bean
-    @Scope(value = "session", proxyMode = ScopedProxyMode.INTERFACES)
+    @Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
     public OAuth2RestTemplate googleRestTemplate() {
-        return new OAuth2RestTemplate(googleResource(), new DefaultOAuth2ClientContext(accessTokenRequest));
+        return new OAuth2RestTemplate(googleResource(),
+                new DefaultOAuth2ClientContext(accessTokenRequest));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.exceptionHandling()
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(clientAuthenticationEntryPoint())
+                .and()
+                .logout()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .fullyAuthenticated()
+                .and()
+                .addFilterAfter(oauth2ClientContextFilter,
+                        ExceptionTranslationFilter.class)
+                .addFilterBefore(oAuth2AuthenticationProcessingFilter(),
+                        FilterSecurityInterceptor.class);
+    }
+
+    @Bean
+    public OAuth2ClientAuthenticationProcessingFilter oAuth2AuthenticationProcessingFilter()
+            throws Exception {
+        OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(
+                "/googleLogin");
+        filter.setRestTemplate(googleRestTemplate());
+        filter.setTokenServices(tokenServices());
+        return filter;
+    }
+
+    @Bean
+    public GoogleTokenServices tokenServices() {
+        GoogleTokenServices tokenServices = new GoogleTokenServices();
+        tokenServices
+                .setCheckTokenEndpointUrl("https://www.googleapis.com/oauth2/v1/tokeninfo");
+        tokenServices.setClientId(env.getProperty("google.client.id"));
+        tokenServices.setClientSecret(env.getProperty("google.client.secret"));
+        tokenServices.setAccessTokenConverter(googleAccessTokenConverter());
+        return tokenServices;
+    }
+
+    @Bean
+    public GoogleAccessTokenConverter googleAccessTokenConverter() {
+        GoogleAccessTokenConverter googleAccessTokenConverter = new GoogleAccessTokenConverter();
+
+        googleAccessTokenConverter
+                .setUserTokenConverter(defaultUserAuthenticationConverter());
+
+        return googleAccessTokenConverter;
+    }
+
+    @Bean
+    public DefaultUserAuthenticationConverter defaultUserAuthenticationConverter() {
+        return new DefaultUserAuthenticationConverter();
+    }
+
+    @Bean
+    public LoginUrlAuthenticationEntryPoint clientAuthenticationEntryPoint() {
+        return new LoginUrlAuthenticationEntryPoint("/googleLogin");
     }
 }
